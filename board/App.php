98569fea38b39af621df390ef1341be9473b67cf
sosuisen <hidekazu.kubota@gmail.com>
2024-11-26 11:35:45 +0900

    fix: separate App from index.php

---
+++ board/App.php
@@ -0,0 +1,50 @@
+<?php
+
+use eftec\bladeone\BladeOne;
+
+// アプリケーションクラス
+class App
+{
+    private PostDAO $postDAO;
+    private BladeOne $blade;
+
+    public function __construct(PostDAO $postDAO)
+    {
+        $this->postDAO = $postDAO;
+        $this->blade = new BladeOne(__DIR__ . '/../templates', __DIR__ . '/../cache');
+    }
+
+    public function handleRequest(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['_method']) && $_POST['_method'] === 'DELETE') {
+                $this->handleDeleteRequest();
+            } else {
+                $this->handlePostRequest();
+            }
+        } else {
+            $this->handleGetRequest();
+        }
+    }
+
+    private function handlePostRequest(): void
+    {
+        $this->postDAO->createPost($_POST['name'], $_POST['title'], $_POST['content']);
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        exit;
+    }
+
+    private function handleDeleteRequest(): void
+    {
+        $this->postDAO->deletePost($_POST['id']);
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+        exit;
+    }
+
+    private function handleGetRequest(): void
+    {
+        $posts = $this->postDAO->getPosts();
+        echo $this->blade->run('board', ['posts' => $posts]);
+        exit;
+    }
+}
