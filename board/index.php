da6ed3911123684a5cf539899e6c5f38772cc30d
sosuisen <hidekazu.kubota@gmail.com>
2024-11-26 11:32:20 +0900

    fix: rm Database.php

--- board/index.php
+++ board/index.php
@@ -1,11 +1,11 @@
 <?php
 set_include_path(__DIR__ . '/../');
 require 'vendor/autoload.php';
-require 'dao/Database.php';
 require 'dao/PostDAO.php';
 
 use eftec\bladeone\BladeOne;
 
+// アプリケーションクラス
 class App
 {
     private PostDAO $postDAO;
@@ -52,11 +52,24 @@ class App
     }
 }
 
-// アプリケーションの実行
+// アプリはここから開始
 try {
-    $pdo = Database::getConnection();
+    // DB接続
+    $config = include __DIR__ . '/../config/config.php';
+    $pdo = new PDO(
+        "mysql:host={$config['DB_HOST']};port={$config['DB_PORT']};dbname={$config['DB_NAME']};charset=utf8mb4",
+        $config['DB_USER'],
+        $config['DB_PASS'],
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+    // Data Access Objectの生成
     $postDAO = new PostDAO($pdo);
+    // アプリケーションの実行
     $app = new App($postDAO);
+    // リクエストの処理
     $app->handleRequest();
 } catch (Exception $e) {
     error_log("Exception caught: " . $e->getMessage());
